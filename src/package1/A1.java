04e4921e8c04586016936269dc7293eff8e7c0fa
Admin <Admin@DESKTOP-OEN4UN3>
2022-08-07 17:47:01 +0530

    hii

--- src/package1/A1.java
+++ src/package1/A1.java
@@ -1,5 +1,16 @@
 package package1;
 
-public class A1 {
+public class A1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hii");
+		
+		System.out.println("Hello");
+		
+		
+		
+		
+	}
 
 }
